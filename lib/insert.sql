6831783d386a79617f9e7f2b5f1ac267f2b5eb94
Wambui <wambuikaranja78@gmail.com>
2025-08-16 16:26:39 +0300

    creates sql

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 20, 'M', 'brown', 'calm', 1),
+('Rowdy', 15, 'M', 'black', 'aggressive', 1),
+('Tabitha', 8, 'F', 'white', 'friendly', 1),
+('Sergeant Brown', 12, 'M', 'brown', 'serious', 1),
+('Melissa', 6, 'F', 'black', 'sweet', 1),
+('Grinch', 11, 'M', 'green', 'grumpy', 0),
+('Wendy', 7, 'F', 'grey', 'gentle', 1),
+(NULL, NULL, NULL, NULL, NULL, NULL); -- unnamed bear
